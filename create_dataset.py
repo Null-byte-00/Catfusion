1a913590f325f0bb45c23f34084aa510ffc79ccf
rafie <amirali.rafie.03@gmail.com>
2024-05-16 06:47:03 +0330

    removed comments

--- create_dataset.py
+++ create_dataset.py
@@ -6,8 +6,6 @@ def create_dataset(data_dir, dataset_name='datasets/cats.pth'):
     transform = transforms.Compose([
         transforms.Resize((64, 64)),
         transforms.ToTensor(),
-        # Normalize the images to the range [-1, 1]
-        #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 
     dataset = datasets.ImageFolder(data_dir, transform=transform)
