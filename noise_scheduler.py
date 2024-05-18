c07b8d08edaf44347cea2bd4f429c4cc7a873840
rafie <amirali.rafie.03@gmail.com>
2024-05-19 01:28:16 +0330

    increased noise in noise scheduler

--- noise_scheduler.py
+++ noise_scheduler.py
@@ -3,7 +3,7 @@ import torch
 def add_noise(tensor, timestep, total_timesteps, device="cpu"):
     how_much_noise = torch.sqrt(1 - ((total_timesteps - timestep) / total_timesteps)).to(device)
 
-    noise = torch.randn_like(tensor) * 0.6 * how_much_noise
+    noise = torch.randn_like(tensor) * 0.7 * how_much_noise
 
     noisy_tensor = tensor + noise
 
--- test_final.py
+++ test_final.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 device = "cuda"
 dataset = torch.load('datasets/cats.pth')
 data = dataset[1][0].to(device)
-total_timesteps = torch.tensor(20).to(device)
+total_timesteps = torch.tensor(100).to(device)
 to_pil = transforms.ToPILImage()
 
 
@@ -22,17 +22,17 @@ def show_images(images=[]):
 
 def test_model(verbose=True):
     model = DiffusionModel(lr=0.01).to(device)
-    for epoch in range(200):
+    for epoch in range(100):
         model = train_sample(model, data, total_timesteps, device, verbose=verbose)
         if verbose:
             print(f"Epoch {epoch}")
 
     process_images = []
-    random_img, _ = add_noise(data.unsqueeze(0), 19 , total_timesteps, device)  #torch.randn(1, 3, 64, 64).clamp(-1, 1).to(device)
+    random_img, _ = add_noise(data.unsqueeze(0), 99 , total_timesteps, device)  #torch.randn(1, 3, 64, 64).clamp(-1, 1).to(device)
     process_images.append(random_img[0].cpu())
     for timestep in range(total_timesteps)[::-1]:
-        random_img = denoise_timestep(model, random_img, timestep, total_timesteps, device, beta=0.15).clamp(-1, 1)
-        if timestep in [0, 4, 10, 14, 18, 20]:
+        random_img = denoise_timestep(model, random_img, timestep, total_timesteps, device, beta=0.025).clamp(-1, 1)
+        if timestep in [0, 20, 50, 70, 90, 100]:
             process_images.append(random_img[0].cpu())
     show_images(process_images)
 
