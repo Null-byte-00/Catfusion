369be54f95cde6691c84b461b80b035cea9ea1b1
rafie <amirali.rafie.03@gmail.com>
2024-05-18 23:27:33 +0330

    fixed final_test.py

--- test_final.py
+++ test_final.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 device = "cuda"
 dataset = torch.load('datasets/cats.pth')
 data = dataset[1][0].to(device)
-total_timesteps = torch.tensor(10).to(device)
+total_timesteps = torch.tensor(20).to(device)
 to_pil = transforms.ToPILImage()
 
 
@@ -21,19 +21,21 @@ def show_images(images=[]):
 
 
 def test_model():
+    model = DiffusionModel(lr=0.01).to(device)
     for epoch in range(500):
-        model = DiffusionModel(lr=0.01)
         model = train_sample(model, data, total_timesteps, device, verbose=True)
         print(f"Epoch {epoch}")
 
     process_images = []
-    random_img, _ = add_noise(data.unsqueeze(0), 99, total_timesteps, device)  #torch.randn(1, 3, 64, 64).clamp(-1, 1).to(device)
-    for timestep in range(100)[::-1]:
+    random_img, _ = add_noise(data.unsqueeze(0), 19 , total_timesteps, device)  #torch.randn(1, 3, 64, 64).clamp(-1, 1).to(device)
+    process_images.append(random_img[0].cpu())
+    for timestep in range(total_timesteps)[::-1]:
         random_img = denoise_timestep(model, random_img, timestep, total_timesteps, device).clamp(-1, 1)
-        if timestep in [0, 2, 5, 7, 9]:
+        if timestep in [0, 4, 10, 14, 18, 20]:
             process_images.append(random_img[0].cpu())
         #print(f"Timestep {timestep}")
     show_images(process_images)
 
 if __name__ == '__main__':
+    model = DiffusionModel(lr=0.01).to(device)
     test_model()
